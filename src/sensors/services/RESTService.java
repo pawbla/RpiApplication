e8624896909aa9fcdbadf2fb7843575964e1ecc4
Pawel <blachut.pawel@gmail.com>
2018-11-26 17:52:25 +0100

    Add RESTService into WebConfig as a bean

--- src/sensors/services/RESTService.java
+++ src/sensors/services/RESTService.java
@@ -8,25 +8,30 @@ import java.util.List;
 
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import homeSystem.EmbeddedApp;
+import sensors.handler.SensorIteratorInterface;
+import sensors.handler.SensorsHandlerInterface;
 
 /**
  * Class to fetch datas using REST Template
  * @author blach
  *
  */
-public abstract class RESTService {
+@Component
+public class RESTService {
 	
 	/**
 	 * Logger
@@ -43,6 +48,10 @@ public abstract class RESTService {
 	private ObjectMapper mapper;
 	private Date date;
 	private DateFormat dateFormat;
+	private SensorIteratorInterface sensorIterator;
+	
+    @Autowired
+    private SensorsHandlerInterface sensorHandler;
 		
 	private final long timeout = 10000;
 	
@@ -60,6 +69,8 @@ public abstract class RESTService {
 	    entity = new HttpEntity<Object>(headers);	
 		map = new HashMap<String,String>();
 		mapper = new ObjectMapper();
+		
+		sensorHandler.getSensorInterfaceIterator();
 	}
 	
 	/**
@@ -67,6 +78,9 @@ public abstract class RESTService {
 	 */
 	@Scheduled(fixedRate = timeout)
 	private void fetchDatas() {
+		while(!sensorIterator.isLastSensorInterface()) {
+			logger.info("Sensor fetched: " + sensorIterator.getSensorInterface().getSensorName());
+		}
 		logger.info("Fetch datas from ip: " + ip);
 		try {
 			resp = rest.exchange("http://" + ip, HttpMethod.GET, entity, String.class);
