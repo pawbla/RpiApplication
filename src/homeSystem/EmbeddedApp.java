98c0a65d5b3a061ed8b789dc56e26e6c210f94b3
Pawel <blachut.pawel@gmail.com>
2020-05-06 22:52:14 +0200

    Remove UI

--- src/homeSystem/EmbeddedApp.java
+++ src/homeSystem/EmbeddedApp.java
@@ -1,16 +1,11 @@
 package homeSystem;
 
-import javax.swing.JFrame;
-import javax.swing.SwingUtilities;
-
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-
-import ui.MainFrame;
 
 @ComponentScan("configurations")
 @EnableAutoConfiguration
@@ -25,12 +20,6 @@ public class EmbeddedApp {
 
         SpringApplication.run(EmbeddedApp.class, args);
        
-        System.setProperty("java.awt.headless", "false");
-        SwingUtilities.invokeLater(() -> {
-        	logger.debug("Initialize MainFrame");
-            MainFrame f = new MainFrame();
-            f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-            f.setVisible(true);
-        });
+        //System.setProperty("java.awt.headless", "false");
 	}	
 }
