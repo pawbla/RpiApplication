72254395040fa7e06815d8aa2090406a7caf3737
Pawel <blachut.pawel@gmail.com>
2020-06-16 19:00:23 +0200

    Small changes

--- src/homeSystem/EmbeddedApp.java
+++ src/homeSystem/EmbeddedApp.java
@@ -17,9 +17,10 @@ public class EmbeddedApp {
 	private static final Logger logger = LogManager.getLogger("EmbeddedApp");
 	
 	public static void main(String[] args) {	
-
+		System.out.println("User home directory: " + System.getProperty("user.home"));
+		System.out.println("User current working directory: " + System.getProperty("user.dir"));
         SpringApplication.run(EmbeddedApp.class, args);
-       
+
         //System.setProperty("java.awt.headless", "false");
 	}	
 }
