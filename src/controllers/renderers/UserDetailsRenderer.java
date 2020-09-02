6782709ebbfe7595fd196161bafd00d84019388a
Pawel <blachut.pawel@gmail.com>
2020-09-02 19:55:09 +0200

    Add additional arguments to user renderer

--- src/controllers/renderers/UserDetailsRenderer.java
+++ src/controllers/renderers/UserDetailsRenderer.java
@@ -18,9 +18,12 @@ public class UserDetailsRenderer {
 	public String getJSON(String login) {
 		Users user = userService.getUserByName(login);
 		response = new JSONObject()
-				.put("login", user.getUserName())
+				.put("user_id", user.getId())
+				.put("username", user.getUserName())
 				.put("firstName", user.getFirstName())
 				.put("lastName", user.getLastName())
+				.put("email", user.getEmail())
+				.put("enabled", user.isEnabled())
 				.put("role", user.getRole().getRole());
 		return response.toString();
 	}
